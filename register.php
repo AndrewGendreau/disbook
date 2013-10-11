2d883404aad49bf314e474f44afe79a5e33cac3b
Ian Englebach <ian.englebach@gmail.com>
2013-10-10 20:25:28 -0400

    Create register.php

---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+
+// CLIENT INFORMATION
+
+    $fname        = htmlspecialchars(trim($_POST['fname']));
+	$lname        = htmlspecialchars(trim($_POST['lname']));
+	$email        = htmlspecialchars(trim($_POST['email']));
+	$password        = htmlspecialchars(trim($_POST['password']));
+	$dob        = htmlspecialchars(trim($_POST['dob']));
+	
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
+	$mysqli= new mysqli("localhost", "root", "password", "fantasyfootball");
+	// for your reference   (host,username,password,dbname);
+		if ($mysqli->connect_errno) 
+	{
+		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+	}
+	
+	
+	//INSERT INTO tbl_students (FirstName, LastName, Grade) VALUES( '$fname', '$lname', '$grade')"
+	$sql= "INSERT INTO DIS_USER_PROFILE(USER_ID, USER_FNAME) VALUES(1, '$fname');";
+	
+	if(!$mysqli->query($sql))
+	{
+		echo"I am here";
+		echo "Query Failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	
+	
+
+
+
+
+
+?>
