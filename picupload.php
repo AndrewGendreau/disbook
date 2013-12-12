65b36ca4bd3c70c9c02933409ed5e6d577ee8c0d
Bait668 <andrewgendreau@yahoo.com>
2013-12-12 00:11:31 -0500

    Update picupload.php

--- picupload.php
+++ picupload.php
@@ -7,29 +7,22 @@ if ($mysqli->connect_errno)
 {
 echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
+//Sql connection
 
 if(isset($_FILES['userFile']) && $_FILES['userFile']['size'] > 0)
+//checks the user file and if the file exists and if it's size is greater then zero
 {
 //$fileName = $_FILES['file']['name'];
-$tmpName  = $_FILES['userFile']['tmp_name'];
-echo $tmpName;
-//$fileSize = $_FILES['file']['size'];
-//$fileType = $_FILES['file']['type'];
+$tmpName  = $_FILES['userFile']['tmp_name'];//assigns the userfile and a tempory name to the file
 
-$fp      = fopen($tmpName, 'r');
-$content = fread($fp, filesize($tmpName));
-$content = addslashes($content);
-fclose($fp);
+$fp      = fopen($tmpName, 'r'); //opens the file in read mode it and assigns it to fp
+$content = fread($fp, filesize($tmpName)); //opens the file and reads it and calls filesize on it to get the size
+$content = addslashes($content); //calls addslashes on the content variable
+fclose($fp); //closes the file
 
-//if(!get_magic_quotes_gpc())
-//{
-  //  $fileName = addslashes($fileName);
-//}
 
-//include 'library/config.php';
-//include 'library/opendb.php';
 $sql = "INSERT INTO DIS_PHOTOS (PHOTO_NAME, PHOTO_DATE, PHOTO_DATA, USER_EMAIL) VALUES ('$tmpName', 'SYSDATE', '$content', '$email')";
-
+//assigns an insert into that inserts the temp name, date, the image and email of the user that uploaded it 
 
 if(!$mysqli->query($sql))
 	{
@@ -37,8 +30,8 @@ if(!$mysqli->query($sql))
 	}
 else
 	{
-
-//include 'library/closedb.php';
+//executes the query and if it failed echos a fail message 
+//otherwise it echos the database's tempory name and a success message
 
 echo "<br>File $tmpName uploaded<br>";
 	}
