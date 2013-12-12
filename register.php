8ec155d0010d47ab31207b2c8be627f74ac53877
Bait668 <andrewgendreau@yahoo.com>
2013-12-12 00:13:49 -0500

    Update register.php

--- register.php
+++ register.php
@@ -4,14 +4,14 @@ if(!$_POST['FName'] | !$_POST['LName'] | !$_POST['RegEmail'] | !$_POST['password
 {
 	echo('You did not fill in a required field!.');
 	header('location: index.html');
-}
-// CLIENT INFORMATION
+} 
+// CLIENT INFORMATION and pointer back to index.html
 
-$fname = htmlspecialchars(trim($_POST['FName']));
-$lname = htmlspecialchars(trim($_POST['LName']));
-$email = htmlspecialchars(trim($_POST['RegEmail']));
-$password = htmlspecialchars(trim($_POST['password']));
-$dob = htmlspecialchars(trim($_POST['birthday']));
+$fname = htmlspecialchars(trim($_POST['FName'])); //strips special characters and assigns the first name
+$lname = htmlspecialchars(trim($_POST['LName'])); //strips special characters and assigns the last name
+$email = htmlspecialchars(trim($_POST['RegEmail']));//strips special characters and assigns the email address
+$password = htmlspecialchars(trim($_POST['password']));//strips special characters and assigns the password
+$dob = htmlspecialchars(trim($_POST['birthday']));//strips special characters and assigns the birthday
 
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -22,21 +22,18 @@ if ($mysqli->connect_errno)
 {
 echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-
+//sql connnection
 
 //INSERT INTO tbl_students (FirstName, LastName, Grade) VALUES( '$fname', '$lname', '$grade')"
 $sql= "INSERT INTO DIS_USER_PROFILE(USER_FNAME, USER_LNAME, USER_BIRTHDATE, USER_EMAIL, USER_PASSWORD) VALUES('$fname', '$lname', '$dob', '$email', '$password' );";
 
 
-//Fix the error message
 if(!$mysqli->query($sql))
 {
 
 echo "Query Failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
-
-
-
+//error if it failed
 
 
 
